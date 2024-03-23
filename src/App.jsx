05fadc760ce562e10f53b36d2784157537111d17
Matteo Ceserani <ceserani@gmail.com>
2024-03-23 11:53:45 +0100

    Static version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function FilterableProductTable() {
+  
+  let prodotti = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div class="FilterableProductTable">
+      
+      <ProductTable prodotti={prodotti} />
+    </div>
   )
 }
 
-export default App
+function ProductRow({ nome, prezzo, stock }) {
+  return (
+    <div class="ProductRow">
+      <span class="Name">{ nome }</span>
+      <span class="Price">{ prezzo }</span>
+    </div>
+  )
+}
+
+function ProductCategoryRow({ categoria }) {
+  return (
+    <div class="ProductCategoryRow">
+      { categoria }
+    </div>
+  )
+} 
+
+function ProductTable({ prodotti }) {
+  let righe = [];
+
+  righe.push(<ProductCategoryRow categoria="Fruits" />);
+  let fruits = prodotti.filter(p => p.category === "Fruits");
+  fruits.forEach(p => righe.push(<ProductRow nome={p.name} prezzo={p.price} />));
+
+  righe.push(<ProductCategoryRow categoria="Vegetables" />);
+  let vegetables = prodotti.filter(p => p.category === "Vegetables");
+  vegetables.forEach(p => righe.push(<ProductRow nome={p.name} prezzo={p.price} />));
+
+  return (
+    <div class="ProductTable">
+      <div class="ProductTableHeader">
+        <span class="Name_h">Nome</span>
+        <span class="Price_h">Prezzo</span>
+      </div>
+      { righe }
+    </div>
+  )
+}
+
+export default  FilterableProductTable;
